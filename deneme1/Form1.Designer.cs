179ab4c579ea37652f4405390a37bd3544636921
Ahmet Fatih Inci <ahmetfatihinci@hotmail.com>
2016-10-06 18:14:25 +0300

    Gambling Interface for Error Related Potentials Detection

---
+++ deneme1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace deneme1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Goldenrod;
+            this.pictureBox2.Location = new System.Drawing.Point(350, 225);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(206, 378);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox4.Location = new System.Drawing.Point(950, 225);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(206, 378);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(400, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Beige;
+            this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.InitialImage = null;
+            this.pictureBox6.Location = new System.Drawing.Point(350, 225);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(206, 378);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox8.BackgroundImage")));
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.Location = new System.Drawing.Point(950, 225);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(206, 378);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(944, 540);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(944, 540);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Interface";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ deneme1/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace deneme1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String DllName);
+
+        [DllImport("kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr hModule, String ProcName);
+
+        [DllImport("kernel32.dll")]
+        private extern static bool FreeLibrary(IntPtr hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool InitializeWinIoType();
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate void SetPortValType(UInt16 PortAddr, UInt32 PortVal, UInt16 Size);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate bool ShutdownWinIoType();
+
+        IntPtr hMod;
+
+        UInt16 address = 16376; //Port address of Lenovo T530 for PCI Parallel Port, check EEG Based BCI Experiments Manual for further info
+
+        int trial = 100;             // total number of trials
+        int balance = 10000;          // $500
+        int clickLimit = 2;         // 2 sec wait after click - timer1
+        int clickCounter = 0;
+        int clickEvent = 0;         // which card is chosen 1-2-3-4
+        int showLimit = 2;          // 2 sec show selected card response  - timer2
+        int showCounter = 0;
+        int balanceLimit = 2;          // 2 sec show balance  - timer3
+        int balanceCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var height = Screen.PrimaryScreen.Bounds.Height;
+            var width = Screen.PrimaryScreen.Bounds.Width;
+
+            pictureBox2.Height = height / 2;
+            pictureBox4.Height = height / 2;
+            pictureBox6.Height = height / 2;
+            pictureBox8.Height = height / 2;
+
+            pictureBox2.Width = 5 * width / 27;
+            pictureBox4.Width = 5 * width / 27;
+            pictureBox6.Width = 5 * width / 27;
+            pictureBox8.Width = 5 * width / 27;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Check pointer size to determine dll type
+            if (IntPtr.Size == 4)
+            {
+                hMod = LoadLibrary("WinIo32.dll");
+            }
+            else if (IntPtr.Size == 8)
+            {
+                hMod = LoadLibrary("WinIo64.dll");
+            }
+            //Error Message
+            if (hMod == IntPtr.Zero)
+            {
+                MessageBox.Show("Can't find WinIo dll");
+                this.Close();
+            }
+            IntPtr pFunc = GetProcAddress(hMod, "InitializeWinIo");
+            if (pFunc != IntPtr.Zero)
+            {
+                InitializeWinIoType InitializeWinIo = (InitializeWinIoType)Marshal.GetDelegateForFunctionPointer(pFunc, typeof(InitializeWinIoType));
+                bool Result = InitializeWinIo();
+                if (!Result)
+                {
+                    MessageBox.Show("Error returned from InitializeWinIo.\nMake sure you are running with administrative privileges and that the WinIo library files are located in the same directory as your executable file.");
+                    FreeLibrary(hMod);
+                    this.Close();
+                }
+            }
+
+            label1.Visible = true;
+            label1.Text = "BALANCE: $" + balance + " TRIAL: " + trial;
+
+            IntPtr send = GetProcAddress(hMod, "SetPortVal");
+            SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+            SetPortVal(address, 120, 1);
+
+            timer3.Start();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e) // 25 
+        {
+            IntPtr send = GetProcAddress(hMod, "SetPortVal");
+            SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+            SetPortVal(address, 50, 1);
+
+            clickEvent = 2;
+            timer1.Start();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e) // 100
+        {
+            IntPtr send = GetProcAddress(hMod, "SetPortVal");
+            SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+            SetPortVal(address, 100, 1);
+
+            clickEvent = 4;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clickCounter = clickCounter + 1;
+
+            if (clickCounter >= clickLimit)
+            {
+                timer1.Stop();
+                clickCounter = 0;
+
+                Random rnd = new Random();
+                int result = rnd.Next(10);
+                trial--;
+
+                if (clickEvent == 2)
+                {
+                    if (result <= 6) // win %70
+                    {
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 150, 1);
+
+                        pictureBox2.BackColor = System.Drawing.Color.Green;
+                        pictureBox6.Visible = false;
+                        balance = balance + 25;
+                    }
+                    else // lose
+                    {
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 200, 1);
+
+                        pictureBox2.BackColor = System.Drawing.Color.Red;
+                        pictureBox6.Visible = false;
+                        balance = balance - 25;
+                    }
+                }
+
+                else if (clickEvent == 4)
+                {
+                    if (result <= 6) // win %70
+                    {
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 180, 1);
+
+                        pictureBox4.BackColor = System.Drawing.Color.Green;
+                        pictureBox8.Visible = false;
+                        balance = balance + 100;
+                    }
+                    else // lose
+                    {
+                        IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                        SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                        SetPortVal(address, 230, 1);
+
+                        pictureBox4.BackColor = System.Drawing.Color.Red;
+                        pictureBox8.Visible = false;
+                        balance = balance - 100;
+                    }
+                }
+
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            showCounter = showCounter + 1;
+
+            if (showCounter >= showLimit)
+            {
+                timer2.Stop();
+                showCounter = 0;
+
+                pictureBox6.Visible = true;
+                pictureBox8.Visible = true;
+
+                label1.Visible = true;
+                label1.Text = "BALANCE: $" + balance + " TRIAL: " + trial;
+                
+                IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                SetPortVal(address, 120, 1);
+
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            balanceCounter = balanceCounter + 1;
+
+            if (balanceCounter >= balanceLimit)
+            {
+                timer3.Stop();
+                balanceCounter = 0;
+
+                label1.Visible = false;
+
+                IntPtr send = GetProcAddress(hMod, "SetPortVal");
+                SetPortValType SetPortVal = (SetPortValType)Marshal.GetDelegateForFunctionPointer(send, typeof(SetPortValType));
+                SetPortVal(address, 10, 1);
+
+                if (trial<=0)
+                {
+                    SetPortVal(address, 250, 1);
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
